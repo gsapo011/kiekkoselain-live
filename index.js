74d83ad8a5a0ce3e9f65962e406065873231a9e5
Sami Pöyhönen <gsapo011@edu.xamk.fi>
2025-05-12 19:23:21 +0300

    fdasfdsfa

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ const suosikitRoutes_1 = __importDefault(require("./routes/suosikitRoutes"));
 const app = (0, express_1.default)();
 const portti = Number(process.env.PORT) || 3110;
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// Palvele staattiset tiedostot (buildattu React-sovellus)
 app.use(express_1.default.static(path_1.default.resolve(__dirname, 'public')));
-app.get('/', (req, res) => {
+// API-reitit
+app.use('/api', kiekotRoutes_1.default); // esim. /api/kiekot
+app.use('/auth', authRoutes_1.default); // esim. /auth/login
+app.use('/api/suosikit', suosikitRoutes_1.default); // esim. /api/suosikit
+// Palauta index.html kaikille muille reiteille (Reactin client-reitit)
+app.get('*', (req, res) => {
     res.sendFile(path_1.default.resolve(__dirname, 'public', 'index.html'));
 });
-app.use(express_1.default.json());
-// Reititykset
-app.use('/api', kiekotRoutes_1.default); // /api/kiekot
-app.use('/auth', authRoutes_1.default); // /auth/login, /auth/register
-app.use('/api/suosikit', suosikitRoutes_1.default); // /api/suosikit
-// Käynnistetään palvelin
+// Käynnistä palvelin
 app.listen(portti, () => {
     console.log(`Palvelin käynnistettiin osoitteeseen http://localhost:${portti}`);
 });
